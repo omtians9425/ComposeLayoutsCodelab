07c8024d07aeedcac261566274e34907070df02e
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2021-03-08 09:38:29 +0900

    Apply style for card

--- app/src/main/java/com/compose/omtians9425/composelayouts/MainActivity.kt
+++ app/src/main/java/com/compose/omtians9425/composelayouts/MainActivity.kt
@@ -3,16 +3,20 @@ package com.compose.omtians9425.composelayouts
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,7 +43,14 @@ fun Greeting(name: String) {
 
 @Composable
 fun PhotographerCard(modifier: Modifier = Modifier) {
-    Row {
+    Row(
+        modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable { /* TODO */ }
+            .padding(16.dp)
+    ) {
         Surface(
             modifier = Modifier.size(50.dp),
             shape = CircleShape,
