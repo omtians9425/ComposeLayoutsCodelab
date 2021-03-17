13e490f2207e8a97830c603c93593527ab829663
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2021-03-17 09:58:30 +0900

    Use layout modifier

--- app/src/main/java/com/compose/omtians9425/composelayouts/MainActivity.kt
+++ app/src/main/java/com/compose/omtians9425/composelayouts/MainActivity.kt
@@ -19,8 +19,12 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.compose.omtians9425.composelayouts.ui.theme.ComposeLayoutsTheme
 
@@ -94,6 +98,22 @@ fun PhotographerCard(modifier: Modifier = Modifier) {
     }
 }
 
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = Modifier.layout { measurable, constraints ->
+    val placeable = measurable.measure(constraints)
+
+    // Check the composable has a first baseline
+    check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+    val firstBaseline = placeable[FirstBaseline]
+
+    val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+    val height = placeable.height + placeableY
+    layout(placeable.width, height) {
+        placeable.placeRelative(0, placeableY)
+    }
+}
+
 @Preview
 @Composable
 fun PhotographerCardPreview() {
@@ -107,5 +127,21 @@ fun PhotographerCardPreview() {
 fun LayoutsCodelabPreview() {
     ComposeLayoutsTheme {
         LayoutsCodelab()
+    }
+}
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    ComposeLayoutsTheme {
+        Text(text = "Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    ComposeLayoutsTheme {
+        Text(text = "Hi there!", Modifier.padding(top = 32.dp))
     }
 }
